f1acc269fffe6ca18045f088e8cec94a1c844d40
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-03-10 19:16:12 +0500

    Add files via upload

---
+++ task2.ts
@@ -0,0 +1,4 @@
+// store a personName in a variable //
+let personName:String = "Rahat Bano";
+
+console.log(`Hi ${personName}, You learn string type variable today.`);
---
+++ task3.ts
@@ -0,0 +1,7 @@
+// store a person's name ia na variable & then print that person's name in lowercase, uppercase and title case //
+
+let person_Name :string = "Rahat";
+
+console.log(person_Name.toLowerCase());
+console.log(person_Name.toUpperCase());
+console.log(person_Name.charAt(0).toUpperCase()+person_Name.slice(1).toLowerCase());
