d653e4a703c2726e02ca3e058aa967c0f43b1a60
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-03-12 21:09:24 +0500

    Add files via upload

---
+++ task8.ts
@@ -0,0 +1,8 @@
+// you should create four lines that look like this:
+//console.log(5 + 3)
+// your output should simply be four line with the number 8 appearing once in each line
+
+console.log(5 + 3);
+console.log(12 - 4);
+console.log(2 * 4);
+console.log(64 / 8)
