ab0e1f94f1259c1c440f3e0aa9ba5687eac9d0a9
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-03-12 21:09:54 +0500

    Add files via upload

---
+++ task9.ts
@@ -0,0 +1,4 @@
+//Store your favorite number in a variable, then using that variable, create a message that reveals your favorite number. Print that message
+
+let favoriteNumber = 5
+console.log(`this is my favorite Number: ${favoriteNumber}`);
