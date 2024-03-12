24b53b4c82725dfe602ff77bda484a5b3f1edd1a
Rahat Bano <160172836+RahatBano58@users.noreply.github.com>
2024-03-12 21:08:45 +0500

    Add files via upload

---
+++ task7.ts
@@ -0,0 +1,18 @@
+// write addition, subtraction, multiplication ,and division operations that each result in the number 8.Be sure to enclose your operations in print statements to see the result//
+
+let num0 = 5
+let num1 = 3
+console.log(num0 + num1);
+
+let num2 = 14
+let num3 = 6
+console.log(num2 - num3);
+
+let num4 = 4
+let num5 = 2
+console.log(num4 * num5);
+
+let num6 = 16
+let num7 = 2
+console.log(num6 / num7);
+
